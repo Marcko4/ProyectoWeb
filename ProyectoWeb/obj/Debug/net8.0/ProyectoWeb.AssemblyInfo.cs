9f4ef84cc39ac1c8fd2022ace17a5efe8f7e00c2
Lizmoncito <lizrovaz1703@gmail.com>
2025-04-28 19:26:49 -0300

    Cambio de detallelibro y modo oscuro

--- ProyectoWeb/obj/Debug/net8.0/ProyectoWeb.AssemblyInfo.cs
+++ ProyectoWeb/obj/Debug/net8.0/ProyectoWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3348a3ab02196f73df540480f5d625910b9ef7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ea4e4649bc3355940857fb0869fb68ccfcab3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
